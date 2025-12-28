b0be1fe8234aac9ba592d48d05d1f19d692db243
김현우 <162122452+HyunwooKiim@users.noreply.github.com>
2025-12-28 09:32:00 +0900

    feat(#1) : ActionHandler 인터페이스 추가 및 액션 처리 표준화

---
+++ src/main/java/gajeman/jagalchi/jagalchiserver/application/action/handler/ActionHandler.java
@@ -0,0 +1,9 @@
+package gajeman.jagalchi.jagalchiserver.application.action.handler;
+
+import gajeman.jagalchi.jagalchiserver.domain.action.Action;
+import gajeman.jagalchi.jagalchiserver.domain.action.ActionAck;
+
+public interface ActionHandler {
+    ActionAck handle(Action action);
+}
+
